5b686417cd929504f215c85919cef7892757aa13
Rondeau79 <rondeau7@gmail.com>
2021-05-30 01:09:01 -0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/lineage_sofiar.mk
+    $(LOCAL_DIR)/xtended_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sofiar-user \
-    lineage_sofiar-userdebug \
-    lineage_sofiar-eng \
+    xtended_sofiar-user \
+    xtended_sofiar-userdebug \
+    xtended_sofiar-eng \
